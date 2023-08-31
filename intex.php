6181860749c5191162bbd7c36423f86a4e7201d4
Sag05 <sag05@duck.com>
2023-08-31 12:04:35 +0200

    Added main file

---
+++ intex.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+        //Comment
+        #Other Comment
+        
+        /*
+        Multi
+        Line
+        Comment        
+        */
+        
+        $text = /* Mid line comment? */ "Hello World!";
+        echo $text . "<br>";
+        
+        $otherText = "water";
+        echo "Why does ". $otherText ."? <br>";
+        echo "$otherText is a thing <br>";
+        echo $text . $otherText. "<br>";
+
+        $num1 = 5;
+        $num2 = 4;
+        echo $num1 + $num2 . "<br>";
+        echo $num1 . $num2 . "<br>";
+
+        ?>
+
+        <form>
+            
+
+
+
+        </form>
+
+
+    </body>
+</html>
