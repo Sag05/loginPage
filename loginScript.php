744d5c75023c63ea1770df9b8e69de8218e13cda
Sag05 <sag05@duck.com>
2024-01-12 12:11:46 +0100

    a

--- loginScript.php
+++ loginScript.php
@@ -2,6 +2,7 @@
     session_start();
     if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
         header("location: home.php");
+        die();
     }
 
     $link = mysqli_connect("localhost", "root", "", "test");
